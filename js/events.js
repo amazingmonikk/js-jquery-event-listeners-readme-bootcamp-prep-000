ff8494b3a9ddd312fc5c0f11618de632302c1e5e
Monika Tadross <monikk.tadross@gmail.com>
2019-02-21 18:26:51 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -3,28 +3,37 @@ function getIt() {
 $(document).ready(function(){
   $('p').on('click', function(){
     alert("Hey!");
-    });
   });
+});
 }
 
 function frameIt() {
 $(document).ready(function(){ 
   $('image.tasty').on('load', function(){
   
-    });
-  });  
+  });
+});  
 }
 
 
 function pressIt() {
- $(document).ready(function(){ 
-   $('form').on('keydown', function(key){
-     if(key.which == 71) {
-       alert('g was pressed');
-     }
-   });
- });  
+$(document).ready(function(){ 
+$('form').on('keydown', function(key){
+  if(key.which == 71) {
+    alert('g was pressed');
+  }
+});
+});  
 }
 
 
-function 
+function submitIt() {
+$(document).ready(function(){ 
+$('form').on('submit', function() {
+  alert('')
+  
+  
+})  
+  
+}); 
+}
